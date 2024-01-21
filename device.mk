ef44db1ea8acac59bd6ef50d6410d020b7475fac
Vikas-V2 <vikasyadavshii5073@gmail.com>
2024-01-21 11:43:21 +0000

    gs201: Add faceunlock support

--- device.mk
+++ device.mk
@@ -35,6 +35,7 @@ include device/google/gs-common/widevine/widevine.mk
 include device/google/gs-common/sota_app/factoryota.mk
 include device/google/gs-common/misc_writer/misc_writer.mk
 include device/google/gs-common/gyotaku_app/gyotaku.mk
+include vendor/google/faceunlock/device.mk
 
 TARGET_BOARD_PLATFORM := gs201
 
